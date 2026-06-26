d615964ab9a1f963bf77ee979f3d55a62e071268
IseAkira <akira.ise.m@gmail.com>
2026-06-26 14:43:59 +0900

    koko

---
+++ 02/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+export default function Form() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const fullName = firstName + '' + lastName;
+
+  function handleFirstNameChange(e) {
+    setFirstName(e.target.value);
+  }
+
+  function handleLastNameChange(e) {
+    setLastName(e.target.value);
+  }
+
+  return (
+    <>
+      <h2>Let check you in </h2>
+      <label>
+        First name:{''}
+        <input value={firstName} onChange={handleFirstNameChange} />
+      </label>
+      <label>
+        Last name:{''}
+        <input value={lastName} onChange={handleLastNameChange} />
+      </label>
+      <p>
+        Your tickect will be:<b>{fullName}</b>
+      </p>
+    </>
+  );
+}
