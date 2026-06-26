28253f9388a61e79af9c9fe8697bcbd9ff899cb2
IseAkira <akira.ise.m@gmail.com>
2026-06-26 14:14:36 +0900

    簡単な設計完了

--- 01/App.tsx
+++ 01/App.tsx
@@ -33,6 +33,31 @@ export default function Form() {
       <p>
         In whitch city is there a bullboard that turns air into drinkable water?
       </p>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={answer}
+          onChange={handleTextareaChange}
+          disabled={status === 'submitting'}
+        />
+        <br />
+        <button disabled={answer.length === 0 || status === 'submitting'}>
+          Submit
+        </button>
+        {error !== null && <p className="Error">{error.message}</p>}
+      </form>
     </>
   );
+}
+
+function submitForm(answer: string) {
+  return new Promise<void>((resolve, reject) => {
+    setTimeout(() => {
+      const shouldError = answer.toLowerCase() != 'lima';
+      if (shouldError) {
+        reject(new Error('Good guess but a wrong answer.'));
+      } else {
+        resolve();
+      }
+    }, 1500);
+  });
 }
