5e43982727a99ca6b99149872e8edecb55407137
IseAkira <akira.ise.m@gmail.com>
2026-06-30 21:44:38 +0900

    react.dev完了

---
+++ 11/Section.tsx
@@ -0,0 +1,10 @@
+import { LevelContext } from './LevelContext.js';
+
+export default function Section({ children }) {
+  const level = useContext(LevelContext);
+  return (
+    <section className="section">
+      <LevelContext value={level + 1}>{children}</LevelContext>
+    </section>
+  );
+}
